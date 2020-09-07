5a577423dcca2e86803ab4e354398361f48bcacc
wilmerbaby <wilmerbaby@gmail.com>
2020-09-07 15:48:47 -0400

    hard coded anwers and questions tried to add introvel

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+var question = document.getElemtryById("questions")
 var answerA = document.getElementById("a");
 var answerB = document.getElementById("b");
 var answerC = document.getElementById("c");
 var answerD = document.getElementById("d");
-var timer = document.getElementById("time");
 var rightWrong = document.getElementById("wr");
+var timeEl = document.getElementById("time");
+
+var count = 40
+var timeEl = setInterval(function(){
+count--;
+if (count > 0){
+   // print you lose page
+}
+
+
+//loop answers
+answerA=["clothesbrand", "yes", "::","true","false" ]
+asnwerB=["videogame","sometimes","()", "false", "true"]
+answerC=["programinglanguge", "no","<>", "", ""]
+answerD=["tvshow", "maybe", "[]","", "" ]
+var questions =  ["what is javascript?", "is javascript case-sensitive?", "what is the syntex for a array", "is java able to change images", "can you photoshop with java"
+ ]
+//hard code
+buttonEl.addEventListener('click',function())
+
+function loadQuestion(){
+document.body.apendChiled(h1el);
+question.setAttribute.textContent = "is java case sensative"
+
+
+answerA.setAttribute.textContent = "yes"
+document.body.apendChiled(answerB)
+answerB.setAttribute.textConttent = "maybe"
+document.body.apendChiled(answerC)
+answerB.setAttribute.textConttent = "no"
+document.body.apendChiled(answerD)
+answerB.setAttribute.textConttent = "sometimes"
+}
+correctAnwer= answerC[1]
+if {buttonA.addEventListener(click,funciton()) !== answerA
+.setInterval --
+}
+
+
+
+
