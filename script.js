777a57164eb8cdec36025d734b1ef05785667dd7
wilmerbaby <wilmerbaby@gmail.com>
2020-09-06 20:19:21 -0400

    added buttons options and assignd them variables

---
+++ script.js
@@ -0,0 +1,6 @@
+var answerA = document.getElementById("a");
+var answerB = document.getElementById("b");
+var answerC = document.getElementById("c");
+var answerD = document.getElementById("d");
+var timer = document.getElementById("time");
+var rightWrong = document.getElementById("wr");
