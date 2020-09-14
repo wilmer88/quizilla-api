ca5af0681f127e86e767298158082a2b51e9b82c
wilmerbaby <wilmerbaby@gmail.com>
2020-09-13 23:01:56 -0400

    after trying all day for loops to make question render.but faild

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # quizilla-api
+
+date:9/8/20
+
+this is a anplication I was asked to create. Was told to make a quize game that asked questions about javascript using DOM manipulation.
+
+The first step that I took was to creat a repository and added html file and a javascript file
+
+Created elments like h1 and buttons and set them inside a container.
+the reason to Create html elements is so they can be easy to manipulate by
+using DOM method
+
+My next step was to assigned variable the targeted the html properties
--- script.js
+++ script.js
@@ -1,56 +1,85 @@
 var timer = document.getElementById("time");
 var questions = document.getElementById("questions");
 var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
-var container = document.getElementsByClassName("container");
-
+var maincontainer = document.getElementById("container");
 var ask = [
   {
-    "what is javascript?": [
-      "clothesbrand",
-      "videogame",
-      "programing language",
-      "tv",
-    ],
+    question: "is javascript a progarming language:",
+    options: ["true", "false"],
+    answer: "true",
   },
-  { "is javascript case-sensitive?": ["maybe", "sometimes", "false", "true"] },
+
   {
-    "what is the syntex for an array": ["programinglanguge", "no", "<>", "[]"],
+    question: "is javascript case sensative:",
+    options: ["yes", "no"],
+    answer: "yes",
   },
   {
-    "is java able to change images": ["tvshow", "maybe", "[]", "true", "false"],
+    question: "is javascript case sensative:",
+    options: ["yes", "no"],
+    answer: "yes",
   },
+  {
+    question: "is javascript a progarming language:",
+    options: ["true", "false"],
+    answer: "true",
+  }
 ];
 
-// function showquestions() {
-//   for (var i = 0; i < ask.length; i++) {
-//     var questionShown = ask[0];
-//   }
-//   showquestions();
+function showquestions(){
+ maincontainer.attributes(null)
 
-buttonA.addEventListener("click", function () {
-  ask.text = ask[0];
+  for (var i = 0; i < ask.length; i++);
+  var currentquestion = ask[i]
+  questions.append.text(currentquestion)
+  function showanswers(){
+buttonA.append(button)
+var buttonB = button
+buttonA.buttonB.tex(ask[i])
+
+maincontainer.addEventListener("click", function (){
+
+  function setTimer(){
+    var timer = 60;
+  var prepareInterval = setInterval(function(){
+    timer--;
+     if (timer === 0){
+       }, 1000
+
+  
 });
+
+
+
+           
+//   {
+
+//     : [
+//       "clothesbrand",
+//       "videogame",
+//       "programing language",
+//       "tv",
+//     ],
+//   },
+//   { "is javascript case-sensitive?": ["maybe", "sometimes", "false", "true"] },
+//   {
+//     "what is the syntex for an array": ["programinglanguge", "no", "<>", "[]"],
+//   },
+//   {
+//     "is java able to change images": ["tvshow", "maybe", "[]", "true", "false"],
+//   },
+// ];
+
 // }
 // questions.textcontent = ask[i];
-// function setTimer(){
-//       var timer = 60;
-//       var prepareInterval = setInterval(function(){
-//        timer--;
-//         if (timer === 0){
-//          }, 1000
+// 
 //        }
-
-//
 
 // var currentaskIndex = 0;
 // function addAndRemove (){
 //    var currentaskIndex = 0;
 //     ask.textContent = ask[currentaskIndex];
 //     //askArray = ask.split("")
-
 //   // .empty
 //      .questions.setAttribute.textContent = ask[currrentaskIndex]
 // // if else
@@ -59,3 +88,6 @@ buttonA.addEventListener("click", function () {
 //     answerB.setAttribuete.textContent == answerB[0]; +
 //     answerC.setAttribute.textConttent == answerC[0]; +
 //    answerD.setAttribute.textConttent == answerD[0];
+// showquestions()
+//   showquestions()/functio;
+//questions.text = ask[0]
