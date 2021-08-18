ab8c9d9fb066386d33fe9ab9f8733a287d68ee20
wilmerbaby <wilmerbaby@gmail.com>
2021-08-18 13:34:11 -0400

    start over

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-var answerBox = document.getElementsByTagName("button");
-var listItem = document.getElementsByTagName("ul");
+var boton = document.createElement("button");
+var listItem = document.getElementsByTagName("ol");
 var listChoice = document.getElementsByTagName("li");
 var clock = document.getElementById("time");
 var questions = document.getElementById("qa");
-var button1 = document.getElementById("bsb");
-var button2 = document.getElementById("nsync");
-var button3 = document.getElementById("98");
-var button4 = document.getElementById("otown");
-var button5 = document.getElementById("newkid");
-var maincontainer = document.getElementById("container");
 
 var respond1 = ["framework", "for loops", "1990", "loki"];
 var respond2 = ["gaming program", "variables", "1995", "c sharp"];
@@ -23,32 +17,23 @@ var ask = [
   "who wrote this code",
 ];
 
-for (var i = 0; i < respond3.length; i++) {
-  answerBox.textContent = respond3[i];
-}
-var listNumber = 1;
-for (var i = 0; i < respond3.length; i++) {
-  listChoice.id = listNumber++;
-}
-for (var i = 0; i < respond3.length; i++) {
-  listChoice.value = answerBox;
-}
-for (var i = 0; i < respond3.length; i++) {
-  listItem.append(listChoice);
-}
+
+
+document.body.append(boton);
+
+// for (var i = 0; i < respond3.length; i++) {
+//   buton.textContent = respond3[i];
+//   listChoice.id = listNumber++;
+
+//   listChoice.textContent = answerBox;
+
+// for (var i = 0; i < respond3.length; i++) {
+//   listItem.append(listChoice);
+// }
 
 var timer = 60;
 
-var currentindex = 0;
-function showoptions() {
-  questions.textContent = ask[currentindex];
-  button5.textContent = respond1[currentindex];
-  button2.textContent = respond2[currentindex];
-  button3.textContent = respond3[currentindex];
-  button4.textContent = respond4[currentindex];
 
-  currentindex++;
-}
 
 function setTimer() {
   var prepareInterval = setInterval(function () {
@@ -62,12 +47,9 @@ function setTimer() {
 function wasClicked() {
   console.log("buttonkl[o] was clicked");
 }
-page.button2.addEventListener("click", function () {
+listChoice.addEventListener("click", function () {
   wasClicked();
   // setTimer();
   // showoptions();
 });
-// button1.addEventListener("click", function () {
-//   button1.style.display = "none";
-//   showoptions();
-// });
+
