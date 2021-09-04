bc03678a8a914e519105e8616cf143df803d10fc
wilmer88 <wilmerbaby@gmail.com>
2021-09-04 01:38:27 -0400

    added hide show conditionals to showoption function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+var qa = document.getElementById("show");
+var initialss = document.getElementById("hide");
 var currentindex = 0;
 var answerBox = document.getElementsByTagName("button");
 var listItem = document.getElementsByTagName("ul");
@@ -11,10 +14,15 @@ var button4 = document.getElementById("otown");
 var maincontainer = document.getElementById("container");
 var timer = 5;
 var started = false; 
+var showinghigscore=false;
 var respond1 = ["framework", "for loops", "1990", "loki"];
 var respond2 = ["gaming program", "variables", "1995", "c sharp"];
 var respond3 = ["scripting language", "functions", "2002", "rugrats"];
 var respond4 = ["programing language", "c++", "1986", "agent carter"];
+
+initialss.style.display="none"; 
+
+
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -24,9 +32,7 @@ function setTime() {
     if(timer === 0) {
       clearInterval(timerInterval);
       clock = ""
-      
     }
-
   }, 1000);
 }
 
@@ -38,19 +44,20 @@ var ask = [
   "who wrote this code",
 ];
 
-
-
-
-
 function showoptions() {
+
   questions.textContent = ask[currentindex];
   button1.textContent = respond1[currentindex];
   button2.textContent = respond2[currentindex];
   button3.textContent = respond3[currentindex];
   button4.textContent = respond4[currentindex];
   currentindex++;
-  console.log(currentindex)
   
+  console.log(currentindex)
+  if(currentindex >= 5){  
+     qa.style.display="none";
+    initialss.style.display="initial"; 
+   } 
 }
 
 
@@ -59,6 +66,7 @@ function wasClicked() {
   currentindex++;
 }
 button1.addEventListener("click", function () {
+  initialss.style.display="none";
   setTime()
   console.log(currentindex)
 if(started == false){
