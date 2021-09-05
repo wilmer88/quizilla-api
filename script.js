9ce312a4d04349372ea15503307922d62cbb3596
wilmer88 <wilmerbaby@gmail.com>
2021-09-05 07:15:45 -0400

    change and deleted code asociated the interval/timer

--- script.js
+++ script.js
@@ -9,20 +9,21 @@ var button2 = document.getElementById("nsync");
 var button3 = document.getElementById("98");
 var button4 = document.getElementById("otown");
 var maincontainer = document.getElementById("container");
-var timer = 60;
+var timer = 5;
 //array called ask with five objects containing two properties
 var ask = [
   {
     pregunta: "is javascript",
-    options: ["framework", "for loops", "1990", "loki"],
+    options: ["framework", "for loops", "1990", "scripting language"],
+    answer: "scripting language"
   },
   {
     pregunta: "is not part of javascript",
-    options: ["gaming program", "variables", "1995", "c sharp"],
+    options: ["Methods", "variables", "objects", "c sharp"],
   },
   {
     pregunta: "year javascript was created",
-    options: ["scripting language", "functions", "2002", "rugrats"],
+    options: ["1995", "1999", "2002", "1984"],
   },
   {
     pregunta: "time travel marvel show",
@@ -33,41 +34,34 @@ var ask = [
     options: ["Bill Gates", "Brendan Eich", "Steve Jobs", "steven Hopkings"],
   },
 ];
-//instance where the timer on the screen begins to count down unsing the timelimit function
-clock.textContent = setInterval(timelimit, 1000);
+function setTime() {
+  var timerInterval = setInterval(function () {
+    timer--;
+    clock.textContent = timer;
+
+    if (timer === 0) {
+      clearInterval(timerInterval);
+      clock = "";
+    }
+  }, 1000);
+}
+
 //this code hides the set highscore html block
 initialss.style.display = "none";
 
-//this function countsdown the timer with a conditional code to esecute for when the timer reaches zero
-function timelimit() {
-  timer--;
-  clock.textContent = timer;
-  if (timer <= 0) {
-    clearInterval(timelimit);
-    clock=""
-    console.log("timer reached 0 value");
-  }
-};
+
 //function that starts quiz
 function comienzaPrueba() {
 console.log("code will execute when ready");
-
+ 
 }
 
 button1.addEventListener("click", function () {
+ 
   console.log("button1 was clicked");
    comienzaPrueba();
+   setTime()
   
 });
 
-// function setTime() {
-//   var timerInterval = setInterval(function () {
-//     timer--;
-//     clock.textContent = timer;
 
-//     if (timer === 0) {
-//       clearInterval(timerInterval);
-//       clock = "";
-//     }
-//   }, 1000);
-// }
